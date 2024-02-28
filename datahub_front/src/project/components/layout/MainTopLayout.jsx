8901596c1c113ffb3b1172e5863968f5197a6dcb
BoYoung00 <qhdud20@naver.com>
2024-02-28 19:41:19 +0900

    24/02/28 메인페이지 수정

--- datahub_front/src/project/components/layout/MainTopLayout.jsx
+++ datahub_front/src/project/components/layout/MainTopLayout.jsx
@@ -4,7 +4,7 @@ import MainTopButtonUI from "../uI/MainTopButtonUI";
 export default function MainTopLayout(){
     return(
         <div className={styles.mainTopLayout}>
-            <h1 className={styles.mainTopText}>웹 서비스를 구현하고 관리해보세요</h1>
+            <div className={styles.mainTopText}>웹 서비스를 구현하고 관리해보세요</div>
             <div className={styles.mainTopButtons}>
                 <MainTopButtonUI buttonName={"프로젝트 생성하러 가기"}/>
                 <MainTopButtonUI buttonName={"협업 소스 관리하러 가기"}/>
